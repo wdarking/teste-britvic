a1561480ae3558a07ab5f18817b1bd53af558484
Gilmar Pereira <wdarking@gmail.com>
2021-02-21 22:58:40 -0300

    Update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // dev user
+        \App\Models\User::factory()->create([
+            'name' => 'Developer',
+            'email' => 'dev@dev.com',
+            'document' => '12345678909',
+            'password' => Hash::make('dev')
+        ]);
 
-        \App\Models\Vehicle::factory(40)->create();
-
-        \App\Models\Reservation::factory(40)->create();
+        \App\Models\Vehicle::factory()->createMany([
+            ['name' => 'Fusca', 'year' => '1960', 'model' => 'A1', 'brand' => 'Volkswagen'],
+            ['name' => 'Ferrari F8', 'year' => '2016', 'model' => 'A1', 'brand' => 'Ferrari'],
+            ['name' => 'Hyundai HB20', 'year' => '2014', 'model' => 'A1', 'brand' => 'Hyundai'],
+        ]);
     }
 }
